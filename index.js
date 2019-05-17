de3e5fc862ff8c9bf5c1b8d967767a056aab15da
msteknoadam <yesilyurt.gok@gmail.com>
2019-05-17 19:00:50 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+exports.__esModule = true;
+var tf = require("@tensorflow/tfjs-node");
+var fittedModel = undefined;
+var model = tf.sequential();
+model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
+var xs = tf.tensor2d([1, 2, 3, 4, 5, 6, 7, 8], [8, 1]);
+var ys = tf.tensor2d([1, 3, 5, 7, 9, 11, 13, 15], [8, 1]);
+xs.print();
+ys.print();
+model.fit(xs, ys, { epochs: 10 }).then(function () {
+    fittedModel = model;
+    var prediction = model
+        .predict(tf.tensor2d([5], [1, 1]))
+        .toString()
+        .slice(11);
+    if (prediction.charAt(prediction.length - 2) === ',') {
+        console.log('Hey');
+        prediction = "" + prediction.substr(0, prediction.length - 2) + prediction.substr(prediction.length - 1, prediction.length);
+    }
+    console.log(JSON.parse(prediction));
+});
