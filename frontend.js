ee963a24138ede27e0074158a6fff768b88bee57
msteknoadam <yesilyurt.gok@gmail.com>
2019-05-18 20:09:08 +0300

    Fix

--- frontend.js
+++ frontend.js
@@ -2,14 +2,14 @@ const ws = new WebSocket('wss://ai.tekno.icu:8080/');
 
 ws.onopen = () => {
 	console.log('Connected.');
-});
+};
 
 ws.onclose = () => {
 	close.log('Disconnected.');
-});
+};
 
 ws.onmessage = data => {
 	console.log('Received: %s', data);
-});
+};
 
 window.ws = ws;
